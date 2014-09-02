5f27af52769a8476757529daba3707e4295b5dc8
Nathan Corvino <nathan@corvino.com>
2014-09-02 07:41:26 -0500

    Rename asset directories and outputs and pick more representative test assets.

--- stitch-all.sh
+++ stitch-all.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
-bin/stitch test-images/ output/test-stitch.png
-bin/stitch blr/world/ output/world.png
-bin/stitch blr/indoors/ output/indoors.png
-bin/stitch blr/entrance/ output/btr-entrance.png
-bin/stitch blr/main/ output/btr-main.png
-bin/stitch blr/1/ output/btr-1.png
-bin/stitch blr/2/ output/btr-2.png
-bin/stitch blr/3/ output/btr-3.png
+bin/stitch blr/overworld-main/ output/overworld-main.png
+bin/stitch blr/overworld-indoors/ output/overworld-indoors.png
+
+bin/stitch blr/underwold-main/ output/underwold-main.png
+bin/stitch blr/underwold-entrance/ output/underwold-entrance.png
+bin/stitch blr/underwold-mini-1/ output/underwold-mini-1.png
+bin/stitch blr/underwold-mini-2/ output/underwold-mini-2.png
+bin/stitch blr/underwold-mini-3/ output/underwold-mini-3.png
